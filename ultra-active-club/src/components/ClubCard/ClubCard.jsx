0fad327960ea35907608187a21810c77510abcc9
kajal-2011 <kajalrekha888@gmail.com>
2023-09-26 16:23:38 +0600

    issue solved

--- ultra-active-club/src/components/ClubCard/ClubCard.jsx
+++ ultra-active-club/src/components/ClubCard/ClubCard.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import "./ClubCard.css";
 
 const ClubCard = ({ blog, addToListHandler }) => {
+  const [isAdded, setIsAdded] = useState(false);
+
   const { name, image, category, description, age, time_required } = blog;
 
+  const colorChangeHandler = () => {
+    addToListHandler(blog);
+    setIsAdded(!isAdded);
+  };
+
+  const buttonText = isAdded ? "added" : "Add to list";
+  const buttonColor = isAdded ? "added-color" : "add-list-btn";
   return (
     <div className="club-card">
       <img src={image} alt={category} className="club-img" />
@@ -12,8 +22,8 @@ const ClubCard = ({ blog, addToListHandler }) => {
         <p>{description}</p>
         <p className="age">For age: {age}</p>
         <p className="time">Time required: {time_required}</p>
-        <button onClick={() => addToListHandler(blog)} className="btn">
-          Add to list
+        <button onClick={colorChangeHandler} className={buttonColor}>
+          {buttonText}
         </button>
       </div>
     </div>
